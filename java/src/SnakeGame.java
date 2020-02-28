d95b311c444718d47a9bc99dc9275bc46842ab8b
Nadia <atiyeh.nadiaa@gmail.com>
2020-02-28 00:21:57 -0700

    finished findTailExahustive method and started findTailRecursive

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SnakeGame {
     private boolean [][] game;
     private int [] headPosition;
@@ -5,18 +7,36 @@ public class SnakeGame {
     private static int recursiveChecks;
 
     public static void main (String [] args){
-        boolean [][] lol = {{false, true , false},
-                            {true, true, true},
-                            {false, true, false}};
+        boolean [][] lol =  {{true, true, true, true, true},
+                {false, false, true, false, true},
+                {true, true, true, false, true},
+                {true, false, false, false, true},
+                {true, true, true, true, true}};
         SnakeGame SG1 = new SnakeGame(lol, 0,0);
-        SG1.snakeNeighbors(0,0);
+        SG1.printBoard();
+        //SG1.snakeNeighbors(1,1);
+        SG1.findTailExhaustive();
+//        SG1.findTailRecursive();
 
     }
 
     public SnakeGame(){
         game = new boolean[1][1];
-
     }
+
+    public static int getExhaustiveChecks(){
+        return exhaustiveChecks;
+    }
+
+    public static int getRecursiveChecks(){
+        return recursiveChecks;
+    }
+
+    private void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
     public SnakeGame(boolean [][] g, int x, int y){
         game = new boolean[g.length][g.length];
         headPosition = new int[2];
@@ -31,15 +51,27 @@ public class SnakeGame {
     }
 
     public int[] findTailExhaustive(){
-        int length = 0;
+        resetCounters();
+        int snakeLength = 0;
         boolean isSnake = true;
+        int snakeyBois = 0;
         for(int i = 0; i < game.length; i++ ){
-            for (int j = 0; j< game.length; j++){
+            for (int j = 0; j < game[i].length; j++){
                 if(game[i][j] == isSnake){
-                    snakeNeighbors(i,j);
-                    length++;
-                    exhaustiveChecks++;
-
+                    snakeyBois += snakeNeighbors(i,j);
+                    if(snakeyBois == 1){
+                        exhaustiveChecks++;
+                        snakeLength = 1;
+                        headPosition[0] = i;
+                        headPosition[1] = j;
+                    }
+                    if(snakeyBois >= 2){
+                        exhaustiveChecks++;
+                        snakeLength++;
+                        // TO - DO get the last tail position to the last true it traces on the snake
+                        headPosition[0] = i ;
+                        headPosition[1] = j;
+                    }
                 }
                 if(game[i][j] == !isSnake){
                     exhaustiveChecks++;
@@ -47,6 +79,8 @@ public class SnakeGame {
             }
         }
 
+        System.out.println(snakeLength);
+        System.out.print(headPosition[0] + "," + headPosition[1]);
         return headPosition;
 
     }
@@ -55,46 +89,90 @@ public class SnakeGame {
         int snakeyBois = 0;
         boolean isSnake = true;
         for(int i = row-1; i <= row +1; i++){
-            if(i >= 0 && i <= game.length && i != row){
+            if(i >= 0 && i <= game.length - 1 && i != row){
                 if(game[i][col] == isSnake){
                     snakeyBois++;
                 }
             }
         }
-        //System.out.println(snakeyBois);
         for (int j = col - 1; j <= col + 1; j++) {
-            if (j >= 0 && j <= game[0].length && j!= col) {
+            if (j >= 0 && j <= game[0].length - 1 && j!= col) {
                 if (game[row][j] == isSnake) {
                     snakeyBois++;
-                    //System.out.println(snakeyBois);
                 }
             }
         }
-        System.out.println(snakeyBois);
         return snakeyBois;
     }
 
-
-
-//    public int snakeNeighbors(int row, int col){
-//        int snakeyBois = 0;
-//        boolean isSnake = true;
-//        for(int i = 0; i < game.length; i++){
-//            for(int j = 0; j < game[i].length; j++){
-//                if (i >= 0 && i <= game.length) {
-//                    if (j >= 0 && j <= game[i].length) {
-//                        if (game[i][j] == isSnake) {
-//                            if (i != row || j != col) {
-//                                snakeyBois++;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        System.out.println(snakeyBois);
-//        return snakeyBois;
+//    public int[] findTailRecursive(){
+//        return findTailRecursive(headPosition, headPosition);
 //    }
 
+//    public int[] findTailRecursive(int [] currentPosition, int [] previousPosition){
+//        resetCounters();
+//        int snakeLength = 0;
+//        boolean isSnake = true;
+//        int i = currentPosition[0];
+//        int j = currentPosition[1];
+//        int k = previousPosition[0];
+//        int l = previousPosition[1];
+//
+//        //up
+//        if((i >= 0) && (game[i +1][j] == isSnake) && ((i!=k )&& (j!=l))){
+//            k = i;
+//            l = j;
+//            i = i + 1;
+//            j = j;
+//            recursiveChecks++;
+//            snakeLength++;
+//            return findTailRecursive(currentPosition,previousPosition);
+//        }
+//        //down
+//        else if((i >= 0 && i <= game.length - 1) && (i!= k) && (j!=l)){
+//            if((game[i-1][j] == isSnake)){
+//                k = i;
+//                l = j;
+//                i = i - 1;
+//                j = j;
+//                recursiveChecks++;
+//                snakeLength++;
+//
+//                return findTailRecursive(currentPosition,previousPosition);
+//            }
+//        }
+//        //right
+//        else if((j >= 0) && (game[i][j+1] == isSnake) && ((i!= k) && (j!= l))){
+//            k = i;
+//            l = j;
+//            i = i;
+//            j = j + 1;
+//            recursiveChecks++;
+//            snakeLength++;
+//
+//            return findTailRecursive(currentPosition,previousPosition);
+//        }
+//
+//        //left
+//        else if((j >= 0 && i <= game[0].length - 1) && (i!= k) && (j!=l)){
+//            if((game[i][j-1] == isSnake)){
+//                k = i;
+//                l = j;
+//                i = i;
+//                j = j - 1;
+//                recursiveChecks++;
+//                snakeLength++;
+//
+//                return findTailRecursive(currentPosition,previousPosition);
+//            }
+//        }
+//        return currentPosition;
+//    }
 
+    public void printBoard(){
+        for(boolean[] i : game){
+            System.out.println(Arrays.toString(i));
+        }
+    }
 }
+
--- java/src/SnakeGameTester.java
+++ java/src/SnakeGameTester.java
@@ -1,5 +1,121 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class SnakeGameTester {
-    public static void main (String [] args){
-        System.out.print("lol");
-    }
+//    @Test
+//    public void testFindTailExauhstive1(){
+//        // Tests the method with normal outputs that should give the tail position of the snake
+//        // Assumes the board is square and the head position is at the starting postion of the array(0,0)
+//        boolean [][] A = {{true, true, true, true, true},
+//                          {false, false, false, false, true},
+//                          {true, true, true, true, true},
+//                          {true, false, false, false, false},
+//                          {true, true, true, true, true}};
+//        SnakeGame sol = new SnakeGame(A,0,0);
+//        // expected output --> {4,4} and a length of 17 which is printed as the method is done looping
+//        int[] B = {4,4};
+//        assertArrayEquals(B, sol.findTailExhaustive());
+//    }
+//
+//    @Test
+//    public void testFindTailExauhstive2(){
+//        // Tests the method with normal inputs that should give the tail position of the snake
+//        // Assumes the board is square and the head position is at a different starting point in the array (1,4)
+//        // and that it has neighbors before the starting point.
+//        boolean [][] A = {{true, true, true, true, true},
+//                          {false, false, false, false, true},
+//                          {true, true, true, true, true},
+//                          {true, false, false, false, false},
+//                          {true, true, true, true, true}};
+//        SnakeGame sol = new SnakeGame(A,1,4);
+//        // expected output --> {4,4} and a length of 17 since the snake is starts before the starting point that is being evaluated
+//        int[] B = {4,4};
+//        assertArrayEquals(B, sol.findTailExhaustive());
+//        //Shows us that the program needs to be modified to handle a case where the point in the snake that is being looked at is not the head position
+//        // and identifies the head and tail positions in this case.
+//    }
+//    @Test
+//    public void testFindTailExauhstive3(){
+//        // Tests the method with normal inputs that should give the tail position of the snake
+//        // Assumes the board is square and the head position is at the start of the array (0,0)
+//        // and that the tail of the snake hits another part of the snake
+//        boolean [][] A = {{true, true, true, true, true},
+//                          {false, false, true, false, true},
+//                          {true, true, true, false, true},
+//                          {true, false, false, false, true},
+//                          {true, true, true, true, true}};
+//        SnakeGame sol = new SnakeGame(A,0,0);
+//        // expected output --> {2,2} and a length of 19 since the snake is starts before the starting point that is being evaluated
+//        // *expected to fail*
+//        int[] B = {2,2};
+//        assertArrayEquals(B, sol.findTailExhaustive());
+//        //Shows us that the program needs to be able to handle a case where if the tail position hits another part of the
+//        //snake it will return the last position it was true.
+//    }
+//    @Test
+//    public void testFindTailExauhstive4(){
+//        // Tests the method with normal inputs that should give the tail position of the snake
+//        // Assumes the board is square and the head position is at starting position in array(0,0)
+//        // and that the board has multiple snakes that have various different tail positions
+//        boolean [][] A = {{true, true, true, true, true, true, true, true},
+//                          {false, false, false, false, false, false, false, false},
+//                          {true, true, true, true, true, false, false, false},
+//                          {true, false, false, false, false, false, false, false},
+//                          {true, true, true, true, true, true, true, true},
+//                          {false, false, false, false, false, false, false, false},
+//                          {true, true, true, true, true ,true, true, false},
+//                          {false, false, false, false, false, true, false, false}};
+//        SnakeGame sol = new SnakeGame(A,0,0);
+//        // expected output --> {0,4} and a length of 8 since the snake that is being evaluated has a starting head position at (0,0)
+//        // and should only take that snake and give the tail position of that specific snake
+//        // expected to fail
+//        int[] B = {0,7};
+//        assertArrayEquals(B, sol.findTailExhaustive());
+//        //Shows us that the program needs to be modified to be able to handle more than one snake, or needs to specify to the user
+//        // that only one continuous snake can be used to play the game.
+//    }
+//
+//    @Test
+//    //EDGE CASE
+//    public void testFindTailExauhstive5() {
+//        // Tests the method with abnormal inputs that should not give the tail position of the snake
+//        // Assumes the board is not square and the head position is at the starting point (0,0)
+//        // Tests with ragged Array, and with neighbors in the space where there is supposed to be a gap
+//        boolean[][] A = {{true, true, true, true, true},
+//                         {false, false, true, false},
+//                         {true, true, true, true},
+//                         {true, false, false, false, false},
+//                         {true, true, true}};
+//        SnakeGame sol = new SnakeGame(A, 1, 4);
+//        // expected output --> {0,4} and a length of 5 since the snake has multiple snakes within the array and the one being evaluated is the first one
+//        // *expected to fail with OutOfBoundsExcpetion*
+//        int[] B = {0, 4};
+//        fail();
+//        //Shows us that the program needs to be modified to handle a case the array is ragged, and/or needs to inform the user that an input such as this will not work
+//        // Also shows us that in order for the game to work properly there must be a one cell gap between each row of snake
+//    }
+//
+//    @Test
+//    //EDGE CASE
+//    public void testFindTailExauhstive5() {
+//        // Tests the method with abnormal inputs that should not give the tail position of the snake
+//        // Assumes the board is not square and the head position is at the starting point (0,0)
+//        // Tests with ragged Array, and with neighbors in the space where there is supposed to be a gap
+//        boolean[][] A = {{true, true, true, true, true},
+//                {false, false, true, false},
+//                {true, true, true, true},
+//                {true, false, false, false, false},
+//                {true, true, true}};
+//        SnakeGame sol = new SnakeGame(A, 1, 4);
+//        // expected output --> {0,4} and a length of 5 since the snake has multiple snakes within the array and the one being evaluated is the first one
+//        // *expected to fail with OutOfBoundsExcpetion*
+//        int[] B = {0, 4};
+//        fail();
+//        //Shows us that the program needs to be modified to handle a case the array is ragged, and/or needs to inform the user that an input such as this will not work
+//        // Also shows us that in order for the game to work properly there must be a one cell gap between each row of snake
+//    }
+
+
 }
